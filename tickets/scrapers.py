4f980d10a2ae24d85a099af3d3b28b2515cae221
shih-ying-yu <59386334+shih-ying-yu@users.noreply.github.com>
2021-05-31 20:46:52 +0800

    eztravel 爬蟲
    增加selenium套件
    只回傳標題/價錢/網址
    (沒有限制回傳幾個 要改可以再改)

--- tickets/scrapers.py
+++ tickets/scrapers.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+my_options = Options()
+my_options.add_argument("--headless")
+driver = webdriver.Chrome(options=my_options)# 不開啟實體瀏覽器
+
 
 # 票券網站抽象類別
 class Website(ABC):
@@ -100,3 +107,44 @@ class Kkday(Website):
                     dict(title=title, link=link, price=price, booking_date=booking_date, star=star, source="https://cdn.kkday.com/m-web/assets/img/favicon.png"))
 
         return result
+
+# Eztravel網站
+class Eztravel(Website):
+
+    def scrape(self):
+
+        result = []  # 回傳結果
+
+        if self.city_name:  # 如果城市名稱非空值
+
+            # 取得傳入城市的所有一日遊票券
+            loc_dict = {'基隆': "KEE", '台北': 'TPE', '桃園': 'TA1', '新竹': 'HSZ', '苗栗': 'MI1', '台中': 'TXG', '彰化': 'ZH1', '南投': 'NA0',
+             '雲林': 'YU1', '嘉義': 'CYI', '台南': 'TNN', '高雄': 'KHH', '屏東': 'PIF', '宜蘭': 'YI0', '花蓮': 'HUN', '台東': 'TTT', '澎湖': 'MZG'}
+
+            if loc_dict.get(self.city_name) != "None":
+
+                loc = loc_dict.get(self.city_name)
+                url = "https://activity.eztravel.com.tw/taiwan/results/" + loc + "/N5?keywords="
+                driver.get(url)
+                time.sleep(2)
+                response = driver.page_source
+                soup =  BeautifulSoup(response, "lxml")
+
+                activities = soup.find_all("div", {"class" : "goods-class clearfix w308"})
+
+
+                for activity in activities:
+ 
+                    # 票券名稱
+                    title = activity.find("h4", {"class": "tkt-title"}).getText().strip()
+ 
+                    # 票券詳細內容連結
+                    link = "https://activity.eztravel.com.tw/taiwan/introduction/" + activity.get("data-prodno")
+ 
+                    # 票券價格
+                    price = activity.find("span", {"data-bind": "text: product.formattedMinSitePrice()"}).getText().strip()
+
+                    result.append(dict(title=title, link=link, price=price, source="Eztravel"))
+
+                driver.quit()
+            return result
