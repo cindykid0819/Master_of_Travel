3c61e5847e74d1e129be795bcfc5ab59bf564ac9
Cheng Yu, Liu <84672482+ww352168@users.noreply.github.com>
2021-06-10 16:39:25 +0800

    已整合三個網站的爬蟲

--- tickets/scrapers.py
+++ tickets/scrapers.py
@@ -17,14 +17,16 @@ driver = webdriver.Chrome(options=my_options)# 不開啟實體瀏覽器
 class Website(ABC):
     def __init__(self, city_name, lower_limit, upper_limit):
         self.city_name = city_name # 城市名稱屬性
-        if lower_limit != '':
-            self.lower_limit = lower_limit
+
+        if lower_limit != '': # 搜尋範圍下限
+            self.lower_limit = int(lower_limit)
         else:
-            self.lower_limit = '0'
-        if upper_limit != '':
-            self.upper_limit = upper_limit
+            self.lower_limit = 0
+        if upper_limit != '': # 搜尋範圍上限
+            self.upper_limit = int(upper_limit)
         else:
-            self.upper_limit = '100000'
+            self.upper_limit = 100000
+
 
     @abstractmethod
     def scrape(self):  # 爬取票券抽象方法
@@ -51,7 +53,6 @@ class Arrangement():
             return "star_value"
         else:
             return "price_value"
-
 
 # KLOOK客路網站
 class Klook(Website):
@@ -105,13 +106,13 @@ class Klook(Website):
                 else :
                     star = "無"
                     star_value = 0
-
-                if ((price_value >= int(self.lower_limit)) and (price_value <= int(self.upper_limit))):
-                	result.append(
-                    	dict(title=title, link=link, price=price, price_value=price_value, booking_date=booking_date, star=star, star_value=star_value, source="https://cdn.klook.com/s/dist_web/assert/desktop/imgs/favicon-098cf2db20.png"))
+                
+                # 根據範圍過濾資料
+                if (price_value >= self.lower_limit and price_value <= self.upper_limit):
+                    result.append(
+                        dict(title=title, link=link, price=price, price_value=price_value, booking_date=booking_date, star=star, star_value=star_value, source="https://cdn.klook.com/s/dist_web/assert/desktop/imgs/favicon-098cf2db20.png"))
  
         return result
-
 
 
 # KKday網站
@@ -120,8 +121,7 @@ class Kkday(Website):
     def city_id(self):
         city_id = {"台北":"00001","新北":"00006","桃園":"00008","新竹":"00009","台中":"00002","嘉義":"00014", \
                    "南投":"00012","台南":"00003","高雄":"00004","屏東":"00015","台東":"00018","花蓮":"00005","宜蘭":"00017", \
-                  "澎湖":"00019","金門":"00020","苗栗":"00010","基隆":"00026","墾丁":"00016","雲林":"00013","小琉球":"00024"}
-        
+                  "澎湖":"00019","金門":"00020","苗栗":"00010","基隆":"00026","墾丁":"00016","雲林":"00013","小琉球":"00024"}        
         
         return city_id[self.city_name]
 
@@ -134,7 +134,7 @@ class Kkday(Website):
             # 取得傳入城市的所有一日遊票券
             driver.get(
                 f"https://www.kkday.com/zh-tw/product/productlist/?page=1&city=A01-001-{self.city_id()}&cat=TAG_4_4&sort=rdesc")
-            time.sleep(2)
+            time.sleep(0.2)
             response = driver.page_source
             soup = BeautifulSoup(response, "lxml")
             
@@ -151,28 +151,32 @@ class Kkday(Website):
                 
                 # 票券價格
                 price = activity.find("h4",{"class" : "text-primary"}).text.strip()
+                # 將文字及標號過濾以排序
                 price_value = int(re.sub("\D","",price))
                 
-                #最早可使用日期
+                # 最早可使用日期
                 booking_date = activity.find("div",{"class" : "product-time-icon"}).text.strip()
                 try:
                     booking_date = booking_date[booking_date.index("2"):]
                 except:
                     booking_date = "明天"
+
                 # 評價(用到雙層爬蟲)
                 driver.get(link)
-                time.sleep(1)
-                re = driver.page_source
-                soup_re = BeautifulSoup(re,"lxml")
+                time.sleep(0.2)
+                response2 = driver.page_source
+                soup_re = BeautifulSoup(response2,"lxml")
                 try:
                     star = soup_re.find("b",{"class":"text-primary"}).string.strip()
                     star_value = int(re.sub("\D","",star))
                 except:
                     star = "無"
                     star_value = 0
-                    
-                result.append(
-                    dict(title=title, link=link, price=price,star = star,booking_date =booking_date, source="https://cdn.kkday.com/m-web/assets/img/favicon.png"))
+                
+                # 根據範圍過濾資料
+                if (price_value >= self.lower_limit and price_value <= self.upper_limit):    
+                    result.append(
+                        dict(title=title, link=link, price=price, price_value=price_value, star=star, star_value=star_value, booking_date=booking_date, source="https://cdn.kkday.com/m-web/assets/img/favicon.png"))
 
         return result
 
@@ -219,9 +223,10 @@ class Eztravel(Website):
                     star = "無"
                     star_value = 0
 
-                    if ((price_value >= int(self.lower_limit)) and (price_value <= int(self.upper_limit))):
-                    	result.append(dict(title=title, link=link, price=price, price_value=price_value, star=star, star_value=star_value,source="https://static.cdn-eztravel.com/assets/images/common/logo.jpg"))
+                    # 根據範圍過濾資料
+                    if (price_value >= self.lower_limit and price_value <= self.upper_limit):
+                        result.append(
+                            dict(title=title, link=link, price=price, price_value=price_value, star=star, star_value=star_value, source="https://static.cdn-eztravel.com/assets/images/common/logo.jpg"))
                  
-
                 #driver.quit()
             return result
--- tickets/views.py
+++ tickets/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from .scrapers import Arrangement, Kkday, Eztravel, Klook
 
 def index(request):
-    arrangement = Arrangement(request.POST.get("sort_order"), request.POST.get("sort_condition"),
-                              request.POST.get("lower_limit"), request.POST.get("upper_limit"))
-    klook = Klook(request.POST.get("city_name"))
-    #kkday = Kkday(request.POST.get("city_name"))
-    eztravel = Eztravel(request.POST.get("city_name"))
+    arrangement = Arrangement(request.POST.get("sort_order"), request.POST.get("sort_condition"))
+
+    klook = Klook(request.POST.get("city_name"), request.POST.get("lower_limit"), request.POST.get("upper_limit"))
+    kkday = Kkday(request.POST.get("city_name"), request.POST.get("lower_limit"), request.POST.get("upper_limit"))
+    eztravel = Eztravel(request.POST.get("city_name"), request.POST.get("lower_limit"), request.POST.get("upper_limit"))
 
     context = {
-        #"tickets": klook.scrape() + eztravel.scrape(), "sortKey": arrangement.sortCondition(), "order": arrangement.sortOrder()
-        "tickets": klook.scrape(), "sortKey": arrangement.sortCondition(), "order": arrangement.sortOrder()
+        "tickets": klook.scrape() + kkday.scrape() + eztravel.scrape(), "sortKey": arrangement.sortCondition(), "order": arrangement.sortOrder()
     }
 
     return render(request, "tickets/index.html", context)
